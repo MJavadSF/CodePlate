2b8b2264c90eb0e17f762b904505abbf4312f039
mjavadsf <mjavadsf72@gmail.com>
2022-07-16 13:28:26 +0430

    chore: remove unnecessary components

--- src/components/Wrapper/index.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/macro';
-
-export const StyledWrapper = (props) => {
-  return <Wrapper>{props.children}</Wrapper>;
-};
-
-const Wrapper = styled.div`
-  padding: 3px;
-`;
